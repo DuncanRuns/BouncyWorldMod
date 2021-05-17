b44dcbf5eaa86062eed7180a045b998439b72853
DuncanRL <59705125+DuncanRL@users.noreply.github.com>
2021-05-17 01:09:44 -0300

    e

--- src/main/java/me/duncanruns/bouncyworld/mixin/EntityMixin.java
+++ src/main/java/me/duncanruns/bouncyworld/mixin/EntityMixin.java
@@ -1,6 +1,7 @@
 package me.duncanruns.bouncyworld.mixin;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,9 +15,11 @@ public abstract class EntityMixin {
     @Shadow
     public abstract boolean isSneaking();
 
+    @Shadow public abstract String toString();
+
     @Inject(at = @At("RETURN"), method = "getJumpVelocityMultiplier", cancellable = true)
     private void getJumpVelocityMultiplierMixin(CallbackInfoReturnable<Float> info) {
-        if(this.isSneaking()){
+        if((Object) this instanceof PlayerEntity && !this.isSneaking()){
             info.setReturnValue(info.getReturnValueF()*4.1f);
         }
     }
